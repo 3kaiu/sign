450d4e7ff9532673bcdd81bb06ae0f7607dae45e
3kaiu <3kaiu@seei.dev>
2024-03-20 23:52:23 +0800

    update

--- mocklogin.js
+++ mocklogin.js
@@ -20,10 +20,11 @@ const puppeteer = require('puppeteer')
 			// 处理响应
 			const responseBody = await response.text()
 			console.log('Response Body:', responseBody)
-			await page.waitForSelector('#fx_checkin_b')
-			await page.click('#fx_checkin_b')
 		}
 	})
+
+	await page.waitForSelector('#fx_checkin_b')
+	await page.click('#fx_checkin_b')
 	// 获取签到结果
 	const result = await page.evaluate(() => {
 		return document.querySelector('#fx_checkin_b').textContent
