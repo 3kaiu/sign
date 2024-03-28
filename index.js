175328be76a16b27e2ed618bbe444a860057d2fe
3kaiu <3kaiu@seei.dev>
2024-03-28 22:28:49 +0800

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const puppeteer = require('puppeteer')
 	})
 	console.log(`登陆结果：${result1}`)
 
-	await page.click('#k_misign_topb #fx_checkin_b')
+	await page.click('#k_misign_topb a')
 
 	const result = await page.evaluate(() => {
 		return document.querySelector('#wp div.paiming.cl .font').innerHTML
